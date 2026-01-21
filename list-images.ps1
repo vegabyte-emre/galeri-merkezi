c2d5762b5a5fc0079a3787fe87e399f9d4d75135
vegabyte-emre <emrenasirbusiness@gmail.com>
2026-01-21 14:03:20 +0300

    fix: Convert admin and panel Dockerfiles to multi-stage build
    Previously Dockerfiles were copying pre-built dist folders which were stale.
    Now they build Nuxt applications during Docker build process ensuring
    latest code is always deployed.

---
+++ list-images.ps1
@@ -0,0 +1,24 @@
+﻿add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustCerts68 : ICertificatePolicy {
+    public bool CheckValidationResult(ServicePoint sp, X509Certificate cert, WebRequest req, int problem) { return true; }
+}
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustCerts68
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$portainerUrl = "https://72.62.115.27:9443"
+$apiKey = "ptr_JdUVeJJw0jI6Dq8SfFesSLlOonyu8yvUn4eR8SPsD6Y="
+$endpointId = 3
+
+$headers = @{ 
+    "X-API-Key" = $apiKey
+    "Content-Type" = "application/json"
+}
+
+$images = Invoke-RestMethod -Uri "$portainerUrl/api/endpoints/$endpointId/docker/images/json" -Headers $headers -Method Get
+
+$images | ConvertTo-Json -Depth 5 | Out-File "images.json"
+Write-Host "Images exported to images.json"
+Write-Host "Image count: $($images.Count)"
